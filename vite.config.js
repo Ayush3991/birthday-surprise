f68ba78acf8ca3d9bbaa13eb831b05184ddd1695
Ayush Jaiswal <ayushrvscse@gmail.com>
2025-12-01 00:40:03 +0530

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// yahan base me /REPO-NAME/ lagana zaroori hai
 export default defineConfig({
+  base: '/birthday-mission/',   // <<-- yahan apne repo ka naam
   plugins: [react()],
 })
